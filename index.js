6e965e2775bd83ebf390bf04a53dc5e785b107f9
Pone Z Pyo <phonezawphyo@gmail.com>
2016-10-17 21:39:30 +0900

    [fix]index.js config

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-const search = require('bindings')('fast_template_matcher.node');
-
-search.findSubImage({
-  source: 'spec/fixtures/screen.png',
-  template: 'spec/fixtures/g.png',
-  matchPercent: 70,
-  maximumMatches: 1,
-  downPyramids: 1,
-  searchExpansion: 15,
-}, function(matches){
-  console.log('Results async: ',matches);
-})
-
-
-var resultsSync = search.findSubImage({
-  source: 'spec/fixtures/screen.png',
-  template: 'spec/fixtures/g.png',
-  matchPercent: 70,
-  maximumMatches: 1,
-  downPyramids: 1,
-  searchExpansion: 15,
-})
-console.log('Results sync: ', resultsSync);
-
-console.log('Ended');
+var binary = require('node-pre-gyp');
+var path = require('path');
+var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
+var binding = require(binding_path);
+module.exports = binding;
