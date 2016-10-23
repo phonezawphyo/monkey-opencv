7dd85f6c9278253e2414156c677820d127c9b613
Phone Zaw Phyo <phonezawphyo@gmail.com>
2016-10-23 23:29:41 +0900

    [add]refactor monkey_algo

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var util = require('util');
 var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
 var async = require('async');
 var bindings = require(binding_path);
+var MonkeyAlgo = require('./monkey_algo');
 
 var Constants = bindings.Constants;
 
@@ -57,127 +58,6 @@ ImageDataStream.prototype.end = function(b){
   });
 }
 
-bindings.MonkeyAlgo = {
-  readImage: bindings.readImage,
-
-  findSubImage: function(options, fn) {
-
-    var readIfNeeded = function(o, key, next) {
-      if (typeof o === 'string') {
-        if (o.length > 0) {
-          bindings.readImage(o, function(err, sourceMat){
-            next(null, sourceMat);
-          });
-        } else {
-          throw new TypeError(`${key} (String, Matrix or Buffer expected)`);
-        }
-      } else if (o instanceof Matrix) {
-        next(null, o);
-      } else if (o instanceof Buffer) {
-          bindings.readImage(o, function(err, sourceMat){
-            next(null, sourceMat);
-          });
-      } else {
-        throw new TypeError(`${key} (String, Matrix or Buffer expected)`);
-      }
-    };
-
-    var execute = function(options, fn) {
-      async.parallel({
-        source: function(next) {
-          readIfNeeded(options.source, 'source', function(err, source) {
-            options.source = source;
-            next(null, null);
-          });
-        },
-        templates: function(next) {
-          if (options.templates instanceof Array) {
-            async.map(options.templates, function(o, next) {
-              readIfNeeded(o, 'templates[]', next);
-            }, function(err, templates) {
-              options.templates = templates;
-              options.template = templates[0];
-              next(null, null);
-            });
-          } else {
-            throw new TypeError('templates: Array expected.');
-          }
-        }
-      }, function(err, results) {
-
-        if (typeof options.source === 'undefined') {
-          throw new TypeError('source (String, Matrix or Buffer expected)');
-        }
-
-        if (typeof options.matchPercent === 'undefined') {
-          options.matchPercent = 70;
-        }
-
-        if (typeof options.maximumMatches === 'undefined') {
-          options.maximumMatches = 1;
-        }
-
-        if (typeof options.downPyramids === 'undefined') {
-          options.downPyramids = 0;
-        }
-
-        if (typeof options.searchExpansion === 'undefined') {
-          options.searchExpansion = 10;
-        }
-
-        if (typeof options.method === 'undefined') {
-          options.method = Constants.TM_SQDIFF_NORMED;
-        }
-
-        ['matchPercent', 'maximumMatches', 'downPyramids', 'searchExpansion', 'method']
-        .forEach(function(key) {
-          if (typeof options[key] !== 'number') {
-            throw new TypeError(`${key} (Number expected)`);
-          }
-        });
-
-        if (options.matchPercent < 0 || options.matchPercent > 100) {
-          throw new TypeError('matchPercent (0 <= n <= 100)');
-        }
-
-        if (options.maximumMatches <= 0) {
-          throw new TypeError('maximumMatches (n > 0)');
-        }
-
-        if (options.downPyramids < 0) {
-          throw new TypeError('downPyramids (n >= 0)');
-        }
-
-        if (options.searchExpansion < 0) {
-          throw new TypeError('searchExpansion (n >= 0)');
-        }
-
-        if (options.method < 0 || options.method > 5) {
-          throw new TypeError('method (0 <= n <= 5)');
-        }
-
-        bindings.findSubImage(
-          options.source,
-          options.templates,
-          options.matchPercent,
-          options.maximumMatches,
-          options.downPyramids,
-          options.searchExpansion,
-          options.method,
-          fn);
-      });
-    };
-
-    if (typeof fn === 'function') {
-      execute(options, fn);
-    } else {
-      return new Promise(function(resolve, reject) {
-        execute(options, function(matches) {
-          resolve(matches);
-        });
-      });
-    }
-  },
-};
+bindings.MonkeyAlgo = MonkeyAlgo;
 
 module.exports = bindings;
